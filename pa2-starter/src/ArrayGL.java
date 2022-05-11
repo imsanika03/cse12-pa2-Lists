a1a534b36651fb99c51f0d1170e286dae541ddb0
Sanika G Bharvirkar <sbharvirkar@UCSD.EDU>
2022-05-10 17:50:48 -0700

    new

--- pa2-starter/src/ArrayGL.java
+++ pa2-starter/src/ArrayGL.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class ArrayGL<E> implements MyList<E> {
 
     E[] elements;
@@ -7,6 +9,51 @@ public class ArrayGL<E> implements MyList<E> {
         this.elements = initialElements;
         this.size = initialElements.length;
     }
+
+	@Override
+	public E[] toArray() {
+		return elements; 
+	}
+
+	@Override
+	public void transformAll(MyTransformer mt) {
+		for (int i = 0; i < elements.length; i++) {
+			if (elements[i] != null) {
+				elements[i] = (E) mt.transformElement(elements[i]); 
+			}
+		}
+		
+	}
+
+	@Override
+	public void chooseAll(MyChooser mc) {
+		int count = 0; 
+		ArrayList<E> arrList = new ArrayList<>(); 
+		for (int i = 0; i < elements.length; i++) {
+			if (elements[i] != null && mc.chooseElement(elements[i])) {
+				arrList.add(elements[i]); 
+				
+				count++; 
+			}
+		}
+		
+		E[] newArr = (E[])(new Object[count+1]); 
+		for (int j = 0; j < newArr.length;j++) {
+			newArr[j] = arrList.get(0); 
+			arrList.remove(arrList.get(0)); 
+			
+		}
+		
+		elements = newArr; 
+		size = newArr.length; 
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return elements.length == 0;
+	}
 
     // Fill in all required methods here
 }
--- pa2-starter/src/LinkedGL.java
+++ pa2-starter/src/LinkedGL.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class LinkedGL<E> implements MyList<E> {
 
     class Node {
@@ -14,8 +16,86 @@ public class LinkedGL<E> implements MyList<E> {
     int size;
 
     public LinkedGL(E[] contents) {
-        // Fill in this constructor
+    	front = null; 
+    	for (E e: contents) {
+    		Node add = new Node(e, front); 
+    		front = add;
+    		
+    	}
+    	size = contents.length; 
+       
     }
+
+	@Override
+	public E[] toArray() {
+		int index = 0; 
+		E[] newArr = (E[])(new Object[size]); 
+		Node first = front; 
+		while(first.next != null) {
+			newArr[index] = first.value; 
+			first = first.next; 
+			index++; 
+			
+			
+		}
+		
+		return newArr; 
+		
+	}
+
+	@Override
+	public void transformAll(MyTransformer mt) {
+		E[] ret = this.toArray(); 
+		for (int i = 0; i < ret.length; i++) {
+			if (ret[i] != null) {
+				ret[i] = (E) mt.transformElement(ret[i]); 
+		
+			}
+		}
+		
+		front = null; 
+    	for (E e: ret) {
+    		Node add = new Node(e, front); 
+    		front = add;
+    		
+    	}
+    	size = ret.length;
+		
+	}
+
+	@Override
+	public void chooseAll(MyChooser mc) {
+		E[] ret = this.toArray(); 
+		ArrayList<E> arr = new ArrayList<>(); 
+		int index = 0; 
+		for (int i = 0; i < ret.length; i++) {
+			if (ret[i] != null && mc.chooseElement(ret[i])) {
+			
+				index++; 
+				arr.add(ret[i]); 
+		
+			}
+		}
+		
+		E[] newr = (E[])(new Object[index+1]); 
+		for (int j = 0; j < newr.length; j++) {
+			newr[j] = arr.get(j); 
+		}
+		
+		front = null; 
+    	for (E e: newr) {
+    		Node add = new Node(e, front); 
+    		front = add;
+    		
+    	}
+    	size = newr.length;
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0; 
+	}
 
     // Fill in all methods
 
